a22db680333873a97ef223f831a75ab07483cf04
博文 <z.bowen66@gmail.com>
2017-02-27 14:40:53 +0800

    小改动

--- HashGallery/gallery.js
+++ HashGallery/gallery.js
@@ -1,14 +1,14 @@
+//通用函数，有点模拟jQ的意思，get('.wrap')、get('#wrap')，把$换成了get;
+function get(selector) {
+    var method=selector.substr(0,1)=='.'?'getElementsByClassName':'getElementById';
+    return document[method](selector.substr(1));//这个方括号是什么姿势？obj[method]();
+}
 //翻面控制
 function turn(ele) {
     var cln=ele.className;
     if (/photo-front/.test(cln)){ cln=cln.replace(/photo-front/,'photo-back')}
     else {cln=cln.replace(/photo-back/,'photo-front')}
     return ele.className=cln;
-}
-//通用函数，有点模拟jQ的意思，get('.wrap')、get('#wrap')，把$换成了get;
-function get(selector) {
-    var method=selector.substr(0,1)=='.'?'getElementsByClassName':'getElementById';
-    return document[method](selector.substr(1));//这个方括号是什么姿势？obj[method]();
 }
 //通用函数，生成范围内随机整数，range=[min,max]
 function random(range) {
@@ -39,7 +39,7 @@ function resort(n){
     var _photos=get('.photo');//_photos不是标准数组，不支持sort()、splice()等函数
     var photos=[];//需要把_photos转化成有序的photos数组；
     for(var s=0;s<_photos.length;s++){//不是标准数组，不能用s in _photos写法
-        // _photo[s].className=_photo[s].className.replace(/\s*photo-center\s*/,' '); //多余的功能：去掉所有photo-center类名
+        _photos[s].className=_photos[s].className.replace(/\s*photo-center\s*/,' ');
         photos.push(_photos[s]);
     }
 
@@ -54,13 +54,13 @@ function resort(n){
     for(var i in photos_left){
         photos_left[i].style.left=random(ranges.left.x)+'px';
         photos_left[i].style.top=random(ranges.left.y)+'px';
-        photos_left[i].style['-webkit-transform']='rotate('+random([0,360])+'deg)';
+        photos_left[i].style['-webkit-transform']='rotate('+random([-180,180])+'deg)';
     }
     for (var j in photos_right){
         // photos[j].style.left='1200px';
         photos[j].style.left=random(ranges.right.x)+'px';
         photos[j].style.top=random(ranges.right.y)+'px';
-        photos_right[j].style['-webkit-transform']='rotate('+random([0,360])+'deg)';
+        photos_right[j].style['-webkit-transform']='rotate('+random([-180,180])+'deg)';
     }
 }
 //计算左右分区范围
@@ -80,3 +80,4 @@ function range() {
     // console.log(range.right.x);
     return range;
 }
+// get('.photo-center')[0].onclick=turn(this);
